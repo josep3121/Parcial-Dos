c6245e39bc70199e4e9fd9aa080b17e22125b215
josep3121 <josepb.ayala@gmail.com>
2021-02-03 08:27:58 -0500

    Menu y pie de Pagina

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Inicio</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Libros</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Editorial</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
